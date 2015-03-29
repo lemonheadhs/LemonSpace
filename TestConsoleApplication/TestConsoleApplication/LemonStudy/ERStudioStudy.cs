57d2369e27679ef966f2f8c17a03682cadcaecd3
lemonheadhs <744640996@qq.com>
2015-03-29 22:45:27 +0800

    新建了一个实验用的控制台应用
    第一个实验的类是以ERStudio为例子进行COM交互的测试方法，暂时还没运行成功

---
+++ TestConsoleApplication/TestConsoleApplication/LemonStudy/ERStudioStudy.cs
@@ -0,0 +1,31 @@
+﻿using ERStudio;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication.LemonStudy
+{
+    public class ERStudioStudy
+    {
+        public static void DoTest()
+        {
+            ERStudio.Application erApp = new ERStudio.Application();
+
+            Diagram currDiag = erApp.ActiveDiagram();
+            Model currModel = currDiag.ActiveModel();
+            SubModel currSModel = currModel.ActiveSubModel();
+
+            dynamic names = new ExpandoObject();
+            dynamic num = new ExpandoObject();
+
+            currSModel.EntityNames(ref names, ref num);
+
+            Console.ReadKey();
+
+        }
+        
+    }
+}
