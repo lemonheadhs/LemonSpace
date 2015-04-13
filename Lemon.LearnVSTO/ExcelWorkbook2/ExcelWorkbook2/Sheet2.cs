fa310cd351ba51037d5e5fd3b4ee76d04735be82
lemonheadhs <744640996@qq.com>
2015-04-13 23:14:27 +0800

    VSTO学习项目 更新

--- Lemon.LearnVSTO/ExcelWorkbook2/ExcelWorkbook2/Sheet2.cs
+++ Lemon.LearnVSTO/ExcelWorkbook2/ExcelWorkbook2/Sheet2.cs
@@ -10,6 +10,7 @@ using Microsoft.VisualStudio.Tools.Applications.Runtime;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using System.Diagnostics;
+using Microsoft.Office.Interop.Excel;
 
 namespace ExcelWorkbook2
 {
@@ -22,6 +23,11 @@ namespace ExcelWorkbook2
                 Trace.WriteLine("lemon");
                 Trace.WriteLine(tr.Address);
                 Trace.WriteLine(cr.ToString());
+            };
+
+            list1.BeforeAddDataBoundRow += (s, arg) => 
+            {
+                Trace.WriteLine("lemon");//在拖动右下角的时候没有引发事件
             };
         }
 
@@ -47,8 +53,26 @@ namespace ExcelWorkbook2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            ListColumn lc = list1.ListColumns[1];
+            Trace.WriteLine(lc.Index);//1    --说明，索引的起点是1不是0
+            Trace.WriteLine(lc.Name);//列1
+
+            //for (int n = 0; n < list1.ListRows.Count; n++)
+            //{
+            //    string content = lc.Range[n].value;
+            //    Trace.WriteLine(content);
+            //}
+            Trace.WriteLine(list1.ListRows.Count);
 
         }
 
+        /*
+         要做验证：
+         1.id列新增时，检查是否是输入了重复的id，验证失败时给出提示
+         2.pid列新增时，检查新增的pid是否是已存在的id并且不是本行的id，验证失败时给出提示
+         
+         
+         
+         */
     }
 }
