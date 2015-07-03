44e203be3f8135d6950ade2c9928cacc50955008
lemonheadhs <744640996@qq.com>
2015-07-03 16:59:14 +0800

    Jumony学习项目 更新

---
+++ LearnJumony/LearnJumony/LearnJumony/Program.cs
@@ -0,0 +1,18 @@
+﻿using LearnJumony.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnJumony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FirstJumonyTest.DoTest();
+            FirstJumonyTest.DoTest2();
+        }
+    }
+}
---
+++ LearnJumony/LearnJumony/LearnJumony/Test/FirstJumonyTest.cs
@@ -0,0 +1,45 @@
+﻿using Ivony.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ivony.Html;
+using System.IO;
+
+namespace LearnJumony.Test
+{
+    public class FirstJumonyTest
+    {
+        public static void DoTest()
+        {
+            var parser = new JumonyParser();
+
+            var document = parser.Parse("<html><body><p class=content>Test Content</body></html>");
+
+            var teststr = document.Render();
+            var teststr2 = document.ToString();
+        }
+
+        public static void DoTest2()
+        {
+            var str = AppDomain.CurrentDomain.BaseDirectory;
+            var str2 = Directory.GetCurrentDirectory();
+
+            var str3 = Path.Combine(str, @"..\..\Resource\发文拟办.html");
+
+            var parser = new JumonyParser();
+
+            var document = parser.LoadDocument(str3);
+
+            var inputs = document.Find("input[type=text]").ToList();
+
+            foreach (var node in inputs)
+            {
+                node.ReplaceWith("<asp:textbox ></asp:textbox>");
+            }
+
+            var r = document.Render();
+        }
+    }
+}
