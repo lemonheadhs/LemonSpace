aa9e988f782dd8fe22dc11d163153fdc380128fc
lemonheadhs <744640996@qq.com>
2015-11-03 16:39:36 +0800

    试验练习

---
+++ Lemon.LearnMVC/LemonAppMVC5/LemonBiz/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonBiz
+{
+    /// <summary>
+    /// 二叉树节点
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BTNode<T>
+    {
+        public T Value { get; set; }
+        public BTNode<T> Left { get; set; }
+        public BTNode<T> Right { get; set; }
+
+        public bool IsLeaf { get { return Left == null && Right == null; } }
+    }
+
+    public class BinaryTree<T> : ICollection<BTNode<T>>
+    {
+        public BTNode<T> Root { get; set; }
+
+        public int Height { get; set; }
+
+        public void Add(BTNode<T> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(BTNode<T> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(BTNode<T>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(BTNode<T> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<BTNode<T>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reverse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BTNode<T>> DoPreOrderTraversal()
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
