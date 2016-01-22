6166e31904f0aaafac4a8df63fcf31458e45fa1d
lemonheadhs <744640996@qq.com>
2016-01-22 23:00:45 +0800

    学习bootstrap

---
+++ BootstrapPlayground/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function () {
+    gulp.src('less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }));
+});
+
+gulp.task('default', function () {
+    gulp.watch('less/*.less', ['less']);
+});
+
