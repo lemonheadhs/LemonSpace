bb76e6414597cb380f03cab001a7e241342e9a0a
lemonheadhs <744640996@qq.com>
2016-01-25 09:42:40 +0800

    同步学习MVC的一些文件

---
+++ Lemon.LearnMVC/LemonAppMVC5/LemonAppMVC5/Models/KVPair.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LemonAppMVC5.Models
+{
+    public class KVPair
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
